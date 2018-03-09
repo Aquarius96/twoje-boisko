eff27212ba07b0fd6d6abc4e6304ea28817d2d39
Oskyy <oso96@vp.pl>
2018-03-09 12:52:47 +0100

    Navbar

--- twoje-boisko-frontend/src/components/App.js
+++ twoje-boisko-frontend/src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Navbar from './Navbar';
 class App extends Component {
   render() {
     return (
       <div className="App container">
-       <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Navbar />    
       </div>
     );
   }
