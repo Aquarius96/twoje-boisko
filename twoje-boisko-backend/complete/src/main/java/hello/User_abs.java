aa9c51736251898ec1dac2767d537d148ee14db5
Dawid Wróbel <dawidk.wrobel@gmail.com>
2018-03-13 20:51:28 +0100

    first_user

---
+++ twoje-boisko-backend/complete/src/main/java/hello/User_abs.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class User_abs {
+
+    private String login;
+    private String password;
+
+    public User_abs(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ twoje-boisko-backend/complete/src/main/java/hello/logowanie.java
@@ -0,0 +1,21 @@
+package hello;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class logowanie {
+
+
+
+    @RequestMapping(value = "/logowanie", method = RequestMethod.GET)
+    public User_abs user_abs(@RequestParam(value="login", required = true) String login,
+                                @RequestParam(value="passwd", required = true) String password) {
+        return new User_abs(login,password);
+    }
+
+    
+}
