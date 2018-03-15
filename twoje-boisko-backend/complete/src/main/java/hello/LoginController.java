cb993c994a46048b63e470b7a3e9be6874bff524
Dawid Wróbel <dawidk.wrobel@gmail.com>
2018-03-15 11:27:19 +0100

    before_sql

--- twoje-boisko-backend/complete/src/main/java/hello/LoginController.java
+++ twoje-boisko-backend/complete/src/main/java/hello/LoginController.java
@@ -14,7 +14,7 @@ public class logowanie {
     @RequestMapping(value = "/logowanie", method = RequestMethod.GET)
     public User_abs user_abs(@RequestParam(value="login", required = true) String login,
                                 @RequestParam(value="passwd", required = true) String password) {
-        return new User_abs(login,password);
+        return new User_abs(login,password); // porownac tego usera z dana userow
     }
 
     
