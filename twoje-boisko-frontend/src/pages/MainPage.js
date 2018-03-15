c4e5d1a96d828a1f81819ed56918575eeefd75f6
Oskyy <oso96@vp.pl>
2018-03-15 12:08:52 +0100

    button

--- twoje-boisko-frontend/src/pages/MainPage.js
+++ twoje-boisko-frontend/src/pages/MainPage.js
@@ -5,7 +5,6 @@ class MainPage extends Component {
   render() {
     return (
       <div className="MainPage container">
-      <h1>Aktualności</h1>
       <div class="news-tab row">
         <div class="col-sm-4"> <News /></div>
         <div class="col-sm-4"> <News /></div>
