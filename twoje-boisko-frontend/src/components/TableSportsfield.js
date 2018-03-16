bf22fa8dd4924a06502982f7eed0244b1fc53250
Aquarius96 <marcinzapadka33@wp.pl>
2018-03-16 18:04:59 +0100

    sortowanie

--- twoje-boisko-frontend/src/components/TableSportsfield.js
+++ twoje-boisko-frontend/src/components/TableSportsfield.js
@@ -6,10 +6,44 @@ class TableSportsfield extends Component {
         super(props);
     }
 
-    sortTable(){
-        
+    sortTable(n){
+      var table, rows, switching, i, x, y, shouldSwitch, asc, switchcount = 0;
+      var table = document.getElementById("myTable");
+      var rows = table.getElementsByTagName("TR");
+      switching = true;
+      asc = true; 
+      while (switching) {
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          if (asc) {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              shouldSwitch= true;
+              break;
+            }
+          } else if (!asc) {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              shouldSwitch= true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          switchcount ++; 
+        } else {
+          if (switchcount == 0 && asc) {
+            asc = false;
+            switching = true;
+          }
+        }
+      }
     }
-  render() {
+    render() {
     var mockTableRows = {
         "one":{"name":'Orlik przy szkole podstawowej nr 6',"address":'Szczytno ul. Polska 12'},
         "two":{"name":"Orlik przy zespole szkół nr 3","address":'Szczytno ul. Polska 12'},
@@ -32,7 +66,7 @@ class TableSportsfield extends Component {
                  <tr>
                     <td>{item.name}</td>
                     <td>{item.address}</td>
-                    <td><button class="button button1">Szczegóły</button></td>
+                    <td><button class="button button1" onClick={() => this.sortTable(0)}>Szczegóły</button></td>
                 </tr>
                 )}
         </table>
