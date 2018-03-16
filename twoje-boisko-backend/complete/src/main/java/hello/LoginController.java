690d34c6de79e9b01c6641f6746e39548fc1bc0a
Dawid Wróbel <dawidk.wrobel@gmail.com>
2018-03-16 18:40:35 +0100

    localDB is probably working :D

--- twoje-boisko-backend/complete/src/main/java/hello/LoginController.java
+++ twoje-boisko-backend/complete/src/main/java/hello/LoginController.java
@@ -1,16 +1,7 @@
 package hello;
 
+import org.springframework.web.bind.annotation.*;
 
-
-import java.beans.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
@@ -23,23 +14,4 @@ public class LoginController {
         return new User_abs(login,password); // porownac tego usera z dana userow
     }
 
-    @RequestMapping(value = "/log")
-    public String test(){
-        String name = "nie dzila";
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://orfi.uwm.edu.pl/~s136308:22", "s136310", "WcotvdIS90fv");
-            Statement smt = (Statement) con.createStatement(); //Create Statement to interact
-            String string = "select * from pracownicy";
-			ResultSet r = ((java.sql.Statement) smt).executeQuery(string);
-            while (r.next()) {
-              name = r.getString("name");
-            }
-            con.close();
-       } catch (Exception e) {
-            e.printStackTrace();
-       }
-       return name;
-    }
-    
 }
