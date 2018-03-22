5d64662d0681bd54061facfe8175b5118c66eee4
Aquarius96 <https://Aquarius96@github.com>
2018-03-22 18:44:01 +0100

    moj profil

--- twoje-boisko-frontend/src/pages/MyProfilePage.js
+++ twoje-boisko-frontend/src/pages/MyProfilePage.js
@@ -3,6 +3,22 @@ import './MyProfilePage.css';
 import $ from 'jquery';
 
 class MyProfilePage extends Component {
+  constructor(props){
+    super(props);
+    this.showEditPassword=this.showEditPassword.bind(this);
+  }
+
+  showEditPassword(){
+    $('#editPassword').toggle();
+    if(document.getElementById("zapisz").innerHTML!="Zapisz hasło"){
+      document.getElementById("zapisz").innerHTML="Zapisz hasło";
+    }
+    else{
+      document.getElementById("zapisz").innerHTML="Zapisz zmiany";
+    }
+    }
+    
+
     render() {
       return (
         <div className="MyProfile container">
@@ -13,6 +29,45 @@ class MyProfilePage extends Component {
         <p class="info"> telefon: </p>
         <p> 518 799 424 </p>
         </div>
+        <div class="row">
+        <div class="profile col-sm-6">
+          <div class="row">
+          <label class="col-sm-2">Imię:</label>
+          <input class="col-sm-10"placeholder="Marcin"></input>
+          </div>
+          <div class="row">
+          <label class="col-sm-2">Nazwisko:</label>
+          <input class="col-sm-10"placeholder="Zapadka"></input>
+          </div>
+          <div class="row">
+          <label class="col-sm-2">Email:</label>
+          <input class="col-sm-10"placeholder="marcinzapadka33@wp.pl"></input>
+          </div>
+          <div class="row">
+          <label class="col-sm-2">Telefon:</label>
+          <input class="col-sm-10"placeholder="518799424"></input>
+          </div>
+        </div>
+        <div class="edit col-sm-6">
+        <button class="przyciskAnuluj" onClick={this.showEditPassword}>Zmień hasło</button>
+        <button id="zapisz" class="przyciskAnuluj">Zapisz zmiany</button>
+        <div id="editPassword">
+        <div class="row">
+          <label class="col-sm-3">Stare hasło:</label>
+          <input type="password"class="col-sm-9"></input>
+          </div>
+          <div class="row">
+          <label class="col-sm-3">Nowe hasło:</label>
+          <input type="password"class="col-sm-9"></input>
+          </div>
+          <div class="row">
+          <label class="col-sm-3">Powtórz hasło:</label>
+          <input type="password"class="col-sm-9"></input>
+          </div>
+        </div>
+        </div>
+        </div>
+        
 
         <table id="ReservationTable">
             <tr class="header">
