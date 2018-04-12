3af8d3d26d07fbc015347e0485d24d35bf238d25
Aquarius96 <https://Aquarius96@github.com>
2018-04-12 11:11:12 +0200

    jwt

--- twoje-boisko-backend/complete/src/main/java/hello/test/Test.java
+++ twoje-boisko-backend/complete/src/main/java/hello/test/Test.java
@@ -5,17 +5,20 @@ import hello.Models.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-@RestController
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/test")
+@RestController
 public class Test{
 
+    @CrossOrigin(origins = "http://localhost:3000/")
     @ResponseBody
     @RequestMapping(value = "/geterro")
     public ResponseEntity<?> test() {
         HttpHeaders responseHeaders = new HttpHeaders();
         return ResponseEntity.ok().headers(responseHeaders).body(new Result("99"));
     }
-
+    @CrossOrigin(origins = "http://localhost:3000/")
     @ResponseBody
     @RequestMapping(value = "/getuser")
     public ResponseEntity<?> test1() {
@@ -23,9 +26,9 @@ public class Test{
         return ResponseEntity.ok().headers(responseHeaders).body(new User(99,"Isard","lsard"));
     }
 
-
-    @ResponseBody
+    @CrossOrigin(origins = "http://localhost:3000/")
     @RequestMapping(value = "/getjwt")
+    @ResponseBody
     public ResponseEntity<?> test2() {
         HttpHeaders responseHeaders = new HttpHeaders();
         return ResponseEntity.ok().headers(responseHeaders).body(new Result("jwt","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJJc2FyZCIsInBhc3N3b3JkIjoiSGFzbG8xMjMiLCJmaXJzdG5hbWUiOiJNYXJjaW4iLCJsYXN0bmFtZSI6IlphcGFka2EiLCJlbWFpbCI6InphcGFka2FAd3AucGwiLCJwaG9uZSI6IjY2NjI0MDgyMyJ9.MWc6WyEwzcrYcGClrE8zsUM5CSrXZRs39Z_i5Z9Q9sY"));
--- twoje-boisko-frontend/src/pages/MainPage.js
+++ twoje-boisko-frontend/src/pages/MainPage.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import './MainPage.css';
 import NewsTab from '../components/NewsTab';
+var jwtDecode = require('jwt-decode');
 class MainPage extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    //var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJJc2FyZCIsInBhc3N3b3JkIjoiSGFzbG8xMjMiLCJmaXJzdG5hbWUiOiJNYXJjaW4iLCJsYXN0bmFtZSI6IlphcGFka2EiLCJlbWFpbCI6InphcGFka2FAd3AucGwiLCJwaG9uZSI6IjY2NjI0MDgyMyJ9.MWc6WyEwzcrYcGClrE8zsUM5CSrXZRs39Z_i5Z9Q9sY';
+
+    //var decoded = jwtDecode(token);
+    //console.log(decoded);
+
+    fetch(`http://localhost:8080/test/getjwt`,{mode:'cors'}) 
+            .then(response => response.json())
+            .then(data =>{
+              console.log(data);
+              console.log(jwtDecode(data.value));
+          })      
+  }
+
   render() {
     return (
       <div className="MainPage container">
