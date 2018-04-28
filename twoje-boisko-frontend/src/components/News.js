a3dafa4a14a9d5681fd7f0f3c44eb2b3d05e8928
Aquarius96 <https://Aquarius96@github.com>
2018-04-28 09:11:01 +0200

    nic

--- twoje-boisko-frontend/src/components/News.js
+++ twoje-boisko-frontend/src/components/News.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './News.css';
 
 class News extends Component {
   render() {
     return (
       <div class="news">
-  
-  
-  <div class="card">
-    
-      <div class="content">
-        <div class="title">
-          <p>Fado</p>
-        </div>
-        <div class="separator"></div>
-        <div class="text">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam, turpis nec sollicitudin euismod,
- sem lorem dignissim libero, eget iaculis lectus tellus sed velit. Curabitur vitae accumsan lacus. Nullam urna ante volutpat.     
-        </div>
-        </div>
-      </div>
-  
-</div>
 
-           );
+        <div class="card">
+
+          <div class="content">
+            <div class="title">
+              <p>Fado</p>
+            </div>
+            <div class="separator"></div>
+            <div class="text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam, turpis
+              nec sollicitudin euismod, sem lorem dignissim libero, eget iaculis lectus tellus
+              sed velit. Curabitur vitae accumsan lacus. Nullam urna ante volutpat.
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    );
   }
 }
 
