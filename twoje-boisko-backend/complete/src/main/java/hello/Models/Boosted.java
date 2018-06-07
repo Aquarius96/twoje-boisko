05789171d72cc6665963f7e48b45a0b0ac0f73f7
wrabel <dawidk.wrobel@gmail.com>
2018-06-07 11:28:53 +0200

    boosted class

--- twoje-boisko-backend/complete/src/main/java/hello/Models/Boosted.java
+++ twoje-boisko-backend/complete/src/main/java/hello/Models/Boosted.java
@@ -1,11 +1,21 @@
 package hello.Models;
 
-public class BoostedRes{
+public class Boosted<T,E>{
+    /*
     public Reservation ob1;
     public SportObject ob2;
 
     public BoostedRes(Reservation ob1_, SportObject ob2_){
         this.ob1 = ob1_;
         this.ob2 = ob2_;
+    }
+    */
+
+    public T e1;
+    public E e2;
+
+    public Boosted(T e1_, E e2_){
+        this.e1 = e1_;
+        this.e2 = e2_;
     }
 }
